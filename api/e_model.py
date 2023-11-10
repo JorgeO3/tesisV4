d377c873e07716dc7c9f8b1718c4a70482048484
Jorge Osorio <joheosmo@gmail.com>
2023-11-09 19:43:22 -0500

    feat(api): add envs and refactorize the code for supported

---
+++ api/e_model.py
@@ -0,0 +1,72 @@
+import os
+import torch
+import joblib
+import numpy as np
+import torch.nn as nn
+
+MODEL_TYPE = "e"
+SCALER_X = os.environ.get("SCALER_X")
+SCALER_Y = os.environ.get("SCALER_Y")
+MODEL_FILE = os.environ.get("MODEL_FILE")
+MODELS_DIR = os.environ.get("MODELS_DIR")
+
+MODEL_PATH = os.path.join(MODELS_DIR, MODEL_TYPE, MODEL_FILE)
+SCALER_X_PATH = os.path.join(MODELS_DIR, MODEL_TYPE, SCALER_X)
+SCALER_Y_PATH = os.path.join(MODELS_DIR, MODEL_TYPE, SCALER_Y)
+
+
+class MLP(nn.Module):
+    """
+    Multilayer Perceptron for regression.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(9, 24),
+            nn.Tanh(),
+            nn.Linear(24, 1),
+        )
+
+    def forward(self, x):
+        """
+        Forward pass
+        """
+        return self.layers(x)
+
+
+class EModel:
+    def __init__(self):
+        self.model = MLP()
+        self.model.load_state_dict(torch.load(MODEL_PATH))
+
+    def generate_tensor(self, data):
+        return torch.tensor(data, dtype=torch.float32)
+
+    def log_transform(self, inputs):
+        return np.log1p(inputs)
+
+    def unlog_transform(self, preds):
+        return np.expm1(preds)
+
+    def normalize_inputs(self, inputs):
+        scaler_X = joblib.load(SCALER_X_PATH)
+        return scaler_X.transform(inputs)
+
+    def unnormalize_predictions(self, preds):
+        scaler_y = joblib.load(SCALER_Y_PATH)
+        preds = preds.numpy().reshape(-1, 1)
+        return scaler_y.inverse_transform(preds)
+
+    def inference(self, X):
+        input = np.array(X)
+        input = self.log_transform(input)
+        input = self.normalize_inputs(input)
+        input = self.generate_tensor(input)
+
+        self.model.eval()
+        with torch.no_grad():
+            y_pred = self.model(input)
+            y_pred = self.unnormalize_predictions(y_pred)
+            y_pred = self.unlog_transform(y_pred)
+            return y_pred
--- api/ts_model.py
+++ api/ts_model.py
@@ -4,8 +4,15 @@ import joblib
 import numpy as np
 import torch.nn as nn
 
-CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-MODEL_FOLDER = os.path.join(CURRENT_DIR, "../trained_models/ts")
+MODEL_TYPE = "ts"
+SCALER_X = os.environ.get("SCALER_X")
+SCALER_Y = os.environ.get("SCALER_Y")
+MODEL_FILE = os.environ.get("MODEL_FILE")
+MODELS_DIR = os.environ.get("MODELS_DIR")
+
+MODEL_PATH = os.path.join(MODELS_DIR, MODEL_TYPE, MODEL_FILE)
+SCALER_X_PATH = os.path.join(MODELS_DIR, MODEL_TYPE, SCALER_X)
+SCALER_Y_PATH = os.path.join(MODELS_DIR, MODEL_TYPE, SCALER_Y)
 
 
 class MLP(nn.Module):
@@ -16,13 +23,9 @@ class MLP(nn.Module):
     def __init__(self):
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Linear(11, 10),
-            nn.LeakyReLU(),
-            nn.Linear(10, 9),
-            nn.ReLU(),
-            nn.Linear(9, 9),
+            nn.Linear(9, 22),
             nn.Tanh(),
-            nn.Linear(9, 1),
+            nn.Linear(22, 1),
         )
 
     def forward(self, x):
@@ -34,26 +37,30 @@ class MLP(nn.Module):
 
 class TSModel:
     def __init__(self):
-        model_path = os.path.join(MODEL_FOLDER, "mlp-model.pth")
         self.model = MLP()
-        self.model.load_state_dict(torch.load(model_path))
+        self.model.load_state_dict(torch.load(MODEL_PATH))
 
     def generate_tensor(self, data):
         return torch.tensor(data, dtype=torch.float32)
 
-    def normalize_inputs(self, X):
-        scaler_X_path = os.path.join(MODEL_FOLDER, "scaler_X.save")
-        scaler_X = joblib.load(scaler_X_path)
-        return scaler_X.transform(X)
+    def log_transform(self, inputs):
+        return np.log1p(inputs)
+
+    def unlog_transform(self, preds):
+        return np.expm1(preds)
+
+    def normalize_inputs(self, inputs):
+        scaler_X = joblib.load(SCALER_X_PATH)
+        return scaler_X.transform(inputs)
 
     def unnormalize_predictions(self, preds):
-        scaler_y_path = os.path.join(MODEL_FOLDER, "scaler_y.save")
-        scaler_y = joblib.load(scaler_y_path)
+        scaler_y = joblib.load(SCALER_Y_PATH)
         preds = preds.numpy().reshape(-1, 1)
         return scaler_y.inverse_transform(preds)
 
     def inference(self, X):
         input = np.array(X)
+        input = self.log_transform(input)
         input = self.normalize_inputs(input)
         input = self.generate_tensor(input)
 
@@ -61,4 +68,5 @@ class TSModel:
         with torch.no_grad():
             y_pred = self.model(input)
             y_pred = self.unnormalize_predictions(y_pred)
+            y_pred = self.unlog_transform(y_pred)
             return y_pred
